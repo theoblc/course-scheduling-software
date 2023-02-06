34f38b5170da26486ef571e82355044d2449c673
Devan <devan.prigent@telecom-sudparis.eu>
2023-02-06 10:33:43 +0100

    Correction bug API

--- backend/app_cours/models.py
+++ backend/app_cours/models.py
@@ -2,9 +2,9 @@ from django.db import models
 from app_seance.models import Seance
 
 class Cours(models.Model):
-   nom = models.CharField(primary_key=True, max_length=50)
+   nom = models.CharField(max_length=50)
    seances = models.ForeignKey(Seance, on_delete=models.CASCADE, null=True)
-   nb_heures = models.IntegerField()
+   nb_heures = models.DecimalField(max_digits=4,decimal_places=1)
 
    def _str_(self):
      return self.code
--- backend/app_module/models.py
+++ backend/app_module/models.py
@@ -7,10 +7,10 @@ class Module(models.Model):
    nom = models.CharField(max_length=50)
    seances = models.ForeignKey(Seance, on_delete=models.CASCADE, null=True, related_name='module')
    cours = models.ForeignKey(Cours, on_delete=models.CASCADE, null=True, related_name='module')
-   nb_heures_total = models.IntegerField()
-   nb_heures_tp = models.IntegerField()
-   nb_heures_be = models.IntegerField()
-   nb_heures_ci = models.IntegerField()
+   nb_heures_total = models.DecimalField(max_digits=4,decimal_places=1)
+   nb_heures_tp = models.DecimalField(max_digits=4,decimal_places=1)
+   nb_heures_be = models.DecimalField(max_digits=4,decimal_places=1)
+   nb_heures_ci = models.DecimalField(max_digits=4,decimal_places=1)
 
    def _str_(self):
      return self.code
